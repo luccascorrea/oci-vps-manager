951a71ac96df3f8ece366b7debe0387d1faf7f3a
Luccas Correa <luccascorrea@estudio89.com.br>
2025-06-10 09:07:46 -0300

    Load settings from absolute path

--- conf.py
+++ conf.py
@@ -1,7 +1,8 @@
 import json
 
 def load_settings():
-    with open("config.json") as f:
+    current_dir = __file__.rsplit('/', 1)[0]
+    with open(f"{current_dir}/config.json", "r") as f:
         return json.load(f)
 
 settings = load_settings()
